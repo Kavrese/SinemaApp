0ef7e92e5a93119426e4bb4170486321795b70a3
Артемий <va2005art@gmail.com>
2020-08-04 14:48:12 +0300

    update to work authentication and registration with check email

--- app/src/main/java/com/example/sinemaapp/classes/FireBaseConnect.java
+++ app/src/main/java/com/example/sinemaapp/classes/FireBaseConnect.java
@@ -10,6 +10,7 @@ import com.example.sinemaapp.model.Film;
 import com.example.sinemaapp.model.User;
 import com.example.sinemaapp.model.Video;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,201 +29,14 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class FireBaseConnect {
-    FirebaseDatabase db = FirebaseDatabase.getInstance();
-    int count_video;
-    int no;
-    JSONArray jsonArray;
-    Film film;
+    boolean result = false;
     int maxResultNow;
     ArrayList<Film> list = new ArrayList<>();
     public FireBaseConnect(){}
     public FireBaseConnect (int maxResult){
         this.maxResultNow = maxResult;
     }
-    public void setNewUser (User user){
-        addValue("user",null,user);
-    }
-
-    public void setNewVideo (Video video){
-        addValue("video",video,null);
-    }
-    private void addValue (final String type, final Video video, final User user){
-        if(type.equals("video")){
-             final DatabaseReference myRef = db.getReference("download_video").child(video.getChannel_name()).child("count_video");
-             final DatabaseReference db_ref = db.getReference("download_video").child(video.getChannel_name()).child("items");
-            db_ref.addChildEventListener(new ChildEventListener() {
-                 @Override
-                 public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                     myRef.setValue((int )snapshot.getChildrenCount());
-                 }
-
-                 @Override
-                 public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                 }
-
-                 @Override
-                 public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-                 }
-
-                 @Override
-                 public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                 }
-
-                 @Override
-                 public void onCancelled(@NonNull DatabaseError error) {
-
-                 }
-             });
-            db_ref.child(video.getId_video()).setValue(video, new DatabaseReference.CompletionListener() {
-                @Override
-                public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-
-                }
-            });
-        }else{
-            DatabaseReference db_ref = db.getReference("users").child("items");
-            DatabaseReference db_ref1 = db.getReference("users");
-            db_ref1.addChildEventListener(new ChildEventListener() {
-                @Override
-                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                    DatabaseReference myRef = db.getReference("users").child("total_count");
-                    int count = (int) snapshot.getChildrenCount();
-                    if(count != 0)
-                    myRef.setValue(String.valueOf(count));
-                }
-
-                @Override
-                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                }
-
-                @Override
-                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-            db_ref.child(String.valueOf(user.getUser_id())).setValue(user, new DatabaseReference.CompletionListener() {
-                @Override
-                public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-                }
-            });
-        }
-    }
-    public int getChildrenCountChannel (String channel){
-        DatabaseReference databaseReference = db.getReference().child("download_video").child(channel).child("items");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                count_video =(int) snapshot.getChildrenCount();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        return count_video;
-    }
-    public int editMaxResult (final int maxResultOld){
-        final DatabaseReference ref = db.getReference("download_video").child("maxResult");
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                int maxResultNow = Integer.parseInt(snapshot.getValue().toString());
-                no = maxResultNow-maxResultOld;
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        return no;
-    }
-    public void setMaxResult (int maxResult,String channel){
-        DatabaseReference ref = db.getReference("download_video").child(channel).child("maxResult");
-        ref.setValue(maxResult);
-    }
-    public Film getVideoBD (String id_video){
-        DatabaseReference databaseReference = db.getReference().child("download_video").child("Chuck Review").child("items").child(id_video);
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                film = snapshot.getValue(Film.class);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        return film;
-    }
-    public JSONArray getObjectNameItems(){
-        DatabaseReference databaseReference = db.getReference().child("download_video").child("Chuck Review").child("items");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Object object = snapshot.getValue();
-                String json = new Gson().toJson(object);
-                try {
-                    JSONObject jsonObject = new JSONObject(json);
-                     jsonArray = jsonObject.names();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        return jsonArray;
-    }
-    public void signIn (String email, String password, final Context context){
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-            Toast.makeText(context, "already signed", Toast.LENGTH_SHORT).show();
-        } else {
-            auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(context, "sign in", Toast.LENGTH_SHORT).show();
-                            }else{
-                                Toast.makeText(context, task.getResult().toString(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-        }
-    }
-    public void outUser (){
+    public void outUser () {
         FirebaseAuth.getInstance().signOut();
-    }
-    public void createNewUser (String email, String password, final Context context){
-        FirebaseAuth.getInstance()
-                .createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Toast.makeText(context, "create", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 }
--- app/src/main/java/com/example/sinemaapp/classes/StartActivity.java
+++ app/src/main/java/com/example/sinemaapp/classes/StartActivity.java
@@ -1,47 +1,146 @@
 package com.example.sinemaapp.classes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sinemaapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
 TextView reg;
-Button enter;
+Button enter,email_good;
 EditText pas,log;
 MotionLayout layout;
+LinearLayout lin,lin_email;
+boolean ver_good;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        lin = findViewById(R.id.lin);
+        lin_email = findViewById(R.id.lin_email);
         pas = findViewById(R.id.password_user);
         log = findViewById(R.id.name_user);
+        email_good = findViewById(R.id.good_email);
         enter = findViewById(R.id.enter);
         reg = findViewById(R.id.reg);
         layout = findViewById(R.id.motionLayout);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(pas.getText().toString().isEmpty()){
+                    Snackbar.make(enter,"Введите пароль", BaseTransientBottomBar.LENGTH_SHORT).show();
+                }else if(log.getText().toString().isEmpty()) {
+                    Snackbar.make(enter,"Введите логин/ник", BaseTransientBottomBar.LENGTH_SHORT).show();
+                }else{
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(log.getText().toString(),pas.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        sendVer(log.getText().toString(),pas.getText().toString());
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Snackbar.make(reg,e.getMessage(),BaseTransientBottomBar.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(pas.getText().toString().isEmpty()){
-                    Snackbar.make(enter,"Введите пароль", BaseTransientBottomBar.LENGTH_SHORT);
+                    Snackbar.make(enter,"Введите пароль", BaseTransientBottomBar.LENGTH_SHORT).show();
                 }else if(log.getText().toString().isEmpty()) {
-                    Snackbar.make(enter,"Введите логин/ник", BaseTransientBottomBar.LENGTH_SHORT);
+                    Snackbar.make(enter,"Введите логин/ник", BaseTransientBottomBar.LENGTH_SHORT).show();
                 }else{
-                    Intent in = new Intent(StartActivity.this,MainActivity.class);
-                    startActivity(in);
-                    finish();
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(log.getText().toString(),pas.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Intent in = new Intent(StartActivity.this,MainActivity.class);
+                                        startActivity(in);
+                                        finish();
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Snackbar.make(reg,e.getMessage(),BaseTransientBottomBar.LENGTH_LONG).show();
+                        }
+                    });
                 }
             }
         });
+    }
+
+    private void sendVer(final String email, final String password) {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        layout.setTransition(R.id.tra4);
+                        layout.transitionToEnd();
+                        lin.setClickable(false);
+                        Button back = findViewById(R.id.back_email);
+                        back.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                layout.transitionToStart();
+                                lin.setClickable(true);
+                            }
+                        });
+                        Snackbar.make(pas,"Письмо отправленно",BaseTransientBottomBar.LENGTH_SHORT).show();
+                        email_good.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                FirebaseAuth.getInstance().signOut();
+                                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (task.isSuccessful()) {
+                                                    ver_good = FirebaseAuth.getInstance().getCurrentUser().isEmailVerified();
+                                                    if(ver_good) {
+                                                        Snackbar.make(reg, "Адрес потверждён", BaseTransientBottomBar.LENGTH_SHORT).show();
+                                                        Intent in = new Intent(StartActivity.this,MainActivity.class);
+                                                        startActivity(in);
+                                                        finish();
+                                                    }else
+                                                        Snackbar.make(reg,"Адрес не потверждён",BaseTransientBottomBar.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                            }
+                        });
+                      }
+                    }
+            });
+        }
     }
 }
